35f5dcfb44a238bc0c30afcd4036b4ea4e9db2d8
zahaale20 <alexcz@outlook.com>
2023-10-11 11:59:16 -0700

    finished project. final commit.

--- command_5.c
+++ command_5.c
@@ -3,9 +3,9 @@
 
 int main(int argc, char *argv[]) {
     int i;
-    for (i = 1; i < argc; i++) {
-        if (argv[i][0] == '-') {
-            printf("%s\n", argv[i]);
+    for (i = 1; i < argc; i++) { // iterate through arguments
+        if (argv[i][0] == '-') { // if they contain '-' character in the first character of the argument...
+            printf("%s\n", argv[i]); // print the command
         }
     }
     return 0;
